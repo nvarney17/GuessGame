6205b9d0a845a53eccfd47561134f6e3d95cdc27
nvarney17 <nvarney@pathfinderstudents.org>
2017-09-21 13:36:01 -0400

    Add files via upload

--- GuessingGame.js
+++ GuessingGame.js
@@ -5,7 +5,6 @@ var Total = 0;
 var Games = 0;
 var Average = 0;
 var playAgain = "y";
-var Turns = 0;
 /*alert("totalTurns= "+totalTurns+" Games= "+Games+" Average= "+Average+" playAgain= "+playAgain+"")*/
 
 /*Start Playing*/
@@ -23,21 +22,20 @@ do {
 		}
 		else if (Guess < Answer){
 			alert('Your guess was too low.');
+		}
 		else if (isNaN(Guess)){
 			Turns = Turns - 1
 		}
-	}
-}
+		}
 	while (Guess != Answer);
 	alert('Congratulations you guessed the number in '+(Turns - 1)+' turns.');
 	alert('You have played '+(Games)+' games so far.');
-	(Total = Total + Turns - 1);
-	(Average = Total / Games);
+	Total = Total + Turns - 1;
+	Average = Total / Games;
 	alert('Your average number of guesses is '+(Average)+' .');
 
-	if (Guess == Answer){
-		playAgain = prompt("Would you like to play again? y=Yes");
-	}
+	playAgain = prompt("Would you like to play again? y=Yes");
+
 }
 while (playAgain == "y");
 /*End Playing*/
